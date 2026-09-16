0a748dc01ad34ea6a10542013cca2a7f3b8949a6
AlessandroPapi-STUDE <G00475903@atu.ie>
2026-09-16 18:03:32 +0100

    Tested book class

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,5 +1,7 @@
 package ie.atu;
 
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main
@@ -7,5 +9,14 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Hello World");
+        Book firstBook = new Book();
+        firstBook.title = "First Book";
+        firstBook.author = "John Lennon";
+        firstBook.pageCount = 412;
+
+        System.out.println("First Book: " + firstBook.title);
+        System.out.println("First Book Author: " + firstBook.author);
+        System.out.println("First Book PageCount: " + firstBook.pageCount);
+        System.out.println("First Book Available: " + firstBook.available);
     }
 }
